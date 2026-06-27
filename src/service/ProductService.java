e093681a0500e30ff67dfd2d3ff6e384e1c20fd8
Jovi <ndyjovienda@gmail.com>
2026-06-27 20:52:58 +0700

    memebuat skeleton ProductService

--- src/service/ProductService.java
+++ src/service/ProductService.java
@@ -1,3 +1,12 @@
+package service;
+
+import java.util.HashMap;
+import java.util.Collection;
+import model.Produk;
+
 public class ProductService {
+
+    private HashMap<String, Produk> katalog = new HashMap<>();
+    private HashMap<String, Integer> hargaKustomisasi = new HashMap<>();
 
 }
