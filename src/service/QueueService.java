7cba0c6b42d878b76fb439f8fae0217a08c4819f
anoramuh <ronahumaira2@gmail.com>
2026-06-28 13:42:16 +0700

    edit queueservice

--- src/service/QueueService.java
+++ src/service/QueueService.java
@@ -1,3 +1,19 @@
 public class QueueService {
-
+    private Queue<Pelanggan> antrean = new LinkedList<>();
+    private int nomorBerikutnya = 1;
+    public void tambahAntrean(String nama) {
+        String nomorAntrean = String.format("A%03d", nomorBerikutnya);
+        nomorBerikutnya++;
+        Pelanggan pelanggan = new Pelanggan(nomorAntrean, nama);
+        antrean.offer(pelanggan);
+    }
+    public Pelanggan layaniPelanggan() {
+        return antrean.poll();
+    }
+    public Queue<Pelanggan> lihatAntrean() {
+        return antrean;
+    }
+    public boolean isKosong() {
+        return antrean.isEmpty();
+    }
 }
