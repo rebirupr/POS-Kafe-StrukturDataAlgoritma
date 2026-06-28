0418a86a6f8b17e215b24cbd915b90bd37c025cd
anoramuh <ronahumaira2@gmail.com>
2026-06-28 13:30:44 +0700

    edit itemkeranjang

--- src/model/ItemKeranjang.java
+++ src/model/ItemKeranjang.java
@@ -1,3 +1,26 @@
+package model;
+import java.util.Map;
 public class ItemKeranjang {
-    
+    private Produk produk;
+    private int jumlah;
+    private Map<String, String> kustomisasi;
+    private double hargaFinal;
+    public ItemKeranjang(Produk produk, int jumlah, Map<String, String> kustomisasi, double hargaFinal) {
+        this.produk = produk;
+        this.jumlah = jumlah;
+        this.kustomisasi = kustomisasi;
+        this.hargaFinal = hargaFinal;
+    }
+    public Produk getProduk() {
+        return produk;
+    }
+    public int getJumlah() {
+        return jumlah;
+    }
+    public Map<String, String> getKustomisasi() {
+        return kustomisasi;
+    }
+    public double getHargaFinal() {
+        return hargaFinal;
+    }
 } 
