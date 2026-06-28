cb6727052c01f8ba552956e2251bcde8d11efe30
anoramuh <ronahumaira2@gmail.com>
2026-06-28 13:16:50 +0700

    edit pelanggan

--- src/model/Pelanggan.java
+++ src/model/Pelanggan.java
@@ -1,3 +1,22 @@
-public class Pelanggan {
+package model;
+
+public class Pelanggan { 
+    // Variabel untuk menyimpan nomor antrean pelanggan.
+    private String nomoAntrean;
+    // Variabel untuk menyimpan nama pelanggan.
+    private String nama;
+
+    public Pelanggan(String nomorAntrean, String nama) {
+        this.nomorAntrean = nomorAntrean;
+        this.nama = nama;
+    }
+
+    public String getNomorAntrean() {
+        return nomorAntrean;
+    }
+
+    public String getNama() {
+        return nama;
+    }
     
 }
